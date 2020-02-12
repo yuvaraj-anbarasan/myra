a244ff3e6a3c09c67042f68a60b75e5353084341
yuvaraj-anbarasan <yuvaraj.klei@gmail.com>
2020-02-12 12:35:39 +0530

    Add getDoneIssues service function

--- app/service/myra.js
+++ app/service/myra.js
@@ -1,6 +1,31 @@
 const axios = require( 'axios' ).default;
 
 class Myra {
+    static getInProgressIssues( ) {
+        return new Promise( ( resolve, reject ) => {
+            let toDo = [ ];
+
+            axios.get( 
+                'https://yuvaraj-anbarasan.atlassian.net/rest/api/2/search?jql=assignee="Yuvaraj Anbarasan" and status="To Do"', 
+                {
+                    headers: {
+                        Authorization: "Basic eXV2YXJhai5rbGVpQGdtYWlsLmNvbTpXa05hRVdMQ1JnZWhMVjFlTkphcjU3OUU="
+                    }
+                }
+            )
+            .then( response => {
+                response.data.issues.map( issue => {
+                    toDo.push( issue.fields.summary );
+                });
+
+                resolve( toDo );
+            })
+            .catch( err => {
+                reject( err );
+            });
+        });
+    }
+    
     static getDoneIssues( ) {
         return new Promise( ( resolve, reject ) => {
             let done = [ ];
