dec88596e02f4f8f466f59156f5136b47a5a2560
yuvaraj-anbarasan <yuvaraj.klei@gmail.com>
2020-02-12 12:49:01 +0530

    Add test route

--- app/app.js
+++ app/app.js
@@ -1,5 +1,15 @@
 const express = require( 'express' );
+const MyraService = require( './service/myra' );
 
 const app = express( );
+
+app.get( '/myra', async ( req, res, next ) => {
+     const { inProgress, done } = await MyraService.getMyIssues( );
+     
+     res.status( 200 ).json({
+        inProgress: inProgress,
+        done: done
+     });
+});
 
 module.exports = app;
