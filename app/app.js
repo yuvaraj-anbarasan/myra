d6f455ecb8e72d688a021c208a884f1b1522a8ce
yuvaraj-anbarasan <yuvaraj.klei@gmail.com>
2020-02-14 12:00:24 +0530

    Send message to slack

--- app/app.js
+++ app/app.js
@@ -1,15 +1,23 @@
 const express = require( 'express' );
 const cron = require( 'node-cron' );
+const axios = require( 'axios' ).default;
 const MyraService = require( './service/myra' );
 
 const app = express( );
 
-const task = cron.schedule( '* 08 11 * * *', async ( ) => {
+const task = cron.schedule( '* 10 11 * * *', async ( ) => {
    const { message } = await MyraService.getMyIssues( );
 
-   console.log( message );
-
-   taskKill( );
+   if ( message ) {
+      axios.post( 'https://hooks.slack.com/services/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', { text: message } )
+      .then( response => { 
+         console.log( 'Message sent :)');
+         taskKill( );
+      } ) 
+      .catch( err => console.log( err ) );
+   } else {
+      console.log( 'Could not send message to slack :)');
+   } 
 }); 
 
 function taskKill( ) {
